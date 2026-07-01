f642de94cda80f6df5c80fa5b6ee3c17b408759b
Ousmane Ouedraogo <oousmane@proton.me>
2026-07-01 12:03:15 +0000

    fix dates of data

--- R/sms.R
+++ R/sms.R
@@ -64,7 +64,7 @@ is_gauge_sms <- function(text = NULL) {
     eg_gh_id = sms_parts[1],
     year     = as.integer(format(d, "%Y")),
     month    = as.integer(format(d, "%m")),
-    day      = as.integer(format(d, "%d")),
+    day      = as.integer(format(d, "%d"))-1,
     value    = value,
     flag     = flag
   )
